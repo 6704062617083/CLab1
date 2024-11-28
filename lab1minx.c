9ecdc12eb3aeed0dd099c60380183ca8ee29fb21
6704062617083 <s6704062617083@email.kmutnb.ac.th>
2024-11-28 18:27:16 +0700

    lab1 No.3 done

--- lab1minx.c
+++ lab1minx.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
 int main() {
-	int num1;
-	printf("Type a num1: \n");
-	scanf("%d" , &num1);
-	int num2;
-	printf("Type a num2: \n");
-	scanf("%d" , &num2);
-	if (num1>num2){
+	int n1 , n2;
+	scanf("%d %d", &n1 , &n2);
+	if (n1>n2){
 		printf("num1 is greater than num2 \n");
 		//diff of num1 and num2
-		printf("%d" , (num1-num2));
+		printf("%d" , (n1-n2));
 	} else {
 		printf("num2 is greater than num1 \n");
 		//diff of num2 and num1
-		printf("%d" , (num2-num1));
+		printf("%d" , (n2-n1));
 	}
 }
