9288ff74d1a94ce950800b335f58bbcc7ef89d03
6704062617083 <s6704062617083@email.kmutnb.ac.th>
2024-11-28 18:29:01 +0700

    lab1 No.4 done

--- lab1minx.c
+++ lab1minx.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main() {
-	int n1 , n2;
-	scanf("%d %d", &n1 , &n2);
+	int n1;
+	scanf("%d", &n1);
+	int n2;
+	scanf("%d", &n2);
 	if (n1>n2){
 		printf("num1 is greater than num2 \n");
 		//diff of num1 and num2
